577991a80e9421e7bb97f9812a0705bd68225fc0
range3 <white.line.oar@gmail.com>
2021-04-21 06:06:47 +0000

    Add thallium_nonblocking

---
+++ targets/apps/thallium_nonblocking/main.cpp
@@ -0,0 +1,64 @@
+#include <fmt/ostream.h>
+#include <fmt/printf.h>
+#include <cxxopts.hpp>
+#include <hello_thallium.hpp>
+#include <iostream>
+#include <ostream>
+#include <thallium.hpp>
+#include <thallium/provider.hpp>
+#include <thallium/request.hpp>
+#include <thallium/serialization/stl/string.hpp>
+#include <thread>
+#include "hello_thallium/point.hpp"
+#include "hello_thallium/unused.hpp"
+#include "hello_thallium/utils.hpp"
+
+namespace tl = thallium;
+namespace ht = hello_thallium;
+
+auto main(int argc, char** argv) -> int {
+  cxxopts::Options options("hello_thallium_server", "Hello Thallium Server");
+  // clang-format off
+  options.add_options()
+    ("h,help", "Print usage")
+    ("a,addr", "Address", cxxopts::value<std::string>()->default_value("tcp://127.0.0.1:1234"))
+    ("S,server", "Server", cxxopts::value<bool>()->default_value("false"))
+    ("s,size", "Payload size(byte)", cxxopts::value<size_t>()->default_value("1"))
+  ;
+  // clang-format on
+
+  auto result = options.parse(argc, argv);
+  if (result.count("help") != 0U) {
+    std::cout << options.help() << std::endl;
+    return 0;
+  }
+
+  if (result["server"].as<bool>()) {
+    // server
+    tl::engine myEngine(result["addr"].as<std::string>(), THALLIUM_SERVER_MODE);
+    std::cout << "Server running at address " << myEngine.self() << std::endl;
+    myEngine.define("sum", [](const tl::request& req, int x, int y) {
+      fmt::print("Computing {} + {}\n", x, y);
+      std::flush(std::cout);
+      req.respond(x + y);
+    });
+
+  } else {
+    // client
+    tl::engine myEngine(ht::protocol(result["addr"].as<std::string>()),
+                        THALLIUM_CLIENT_MODE);
+    tl::remote_procedure sum = myEngine.define("sum");
+    tl::endpoint server = myEngine.lookup(result["addr"].as<std::string>());
+    auto request = sum.on(server).async(42, 63); //NOLINT
+    // do something else ...
+    // check if request completed
+    bool completed = request.received();
+    UNUSED(completed);
+    // ...
+    // actually wait on the request and get the result out of it
+    int ret = request.wait();
+    std::cout << "Server answered " << ret << std::endl;
+  }
+
+  return 0;
+}
---
+++ targets/apps/thallium_with_argobot_pools copy/main.cpp
@@ -0,0 +1,80 @@
+#include <fmt/ostream.h>
+#include <fmt/printf.h>
+#include <chrono>
+#include <cxxopts.hpp>
+#include <hello_thallium.hpp>
+#include <iostream>
+#include <ostream>
+#include <thallium.hpp>
+#include <thallium/provider.hpp>
+#include <thallium/request.hpp>
+#include <thallium/serialization/stl/string.hpp>
+#include <thread>
+#include "hello_thallium/utils.hpp"
+
+namespace tl = thallium;
+namespace ht = hello_thallium;
+
+auto main(int argc, char** argv) -> int {
+  cxxopts::Options options("thallium_with_argobot_pools",
+                           "Argobot pools with thallium RPC");
+  // clang-format off
+  options.add_options()
+    ("h,help", "Print usage")
+    ("a,addr", "Address", cxxopts::value<std::string>()->default_value("tcp"))
+    ("S,server", "Server", cxxopts::value<bool>()->default_value("false"))
+  ;
+  // clang-format on
+
+  auto result = options.parse(argc, argv);
+  if (result.count("help") != 0U) {
+    std::cout << options.help() << std::endl;
+    return 0;
+  }
+
+  if (result["server"].as<bool>()) {
+    // server
+    // https://mochi.readthedocs.io/en/latest/thallium/12_rpc_pool.html
+    // > This feature requires to provide a non-zero provider id (passed to the
+    // > define call) when defining the RPC (here 1). Hence you also need to use
+    // > provider handles on clients, even if you do not define a provider
+    // > class.
+
+    tl::abt scope;
+    tl::engine myEngine(result["addr"].as<std::string>(), THALLIUM_SERVER_MODE);
+    std::vector<tl::managed<tl::xstream>> ess;
+    tl::managed<tl::pool> myPool = tl::pool::create(tl::pool::access::spmc);
+    for (size_t i = 0; i < 4; i++) {
+      tl::managed<tl::xstream> es =
+          tl::xstream::create(tl::scheduler::predef::deflt, *myPool);
+      ess.push_back(std::move(es));
+    }
+
+    std::cout << "Server running at address " << myEngine.self() << std::endl;
+
+    myEngine.define(
+        "sum",
+        [](const tl::request& req, int x, int y) {
+          fmt::print("Computing {} + {}\n", x, y);
+          std::flush(std::cout);
+          req.respond(x + y);
+        },
+        0, *myPool);  // provider_id == 0 ? but it works.
+
+    myEngine.wait_for_finalize();
+
+    for (size_t i = 0; i < 4; i++) {
+      ess[i]->join();
+    }
+
+  } else {
+    const auto protocol = ht::protocol(result["addr"].as<std::string>());
+    tl::engine myEngine(protocol, THALLIUM_CLIENT_MODE);
+    tl::remote_procedure sum = myEngine.define("sum");
+    tl::endpoint server = myEngine.lookup(result["addr"].as<std::string>());
+    int ret = sum.on(server)(42, 63);  // NOLINT
+    std::cout << "Server answered " << ret << std::endl;
+  }
+
+  return 0;
+}
