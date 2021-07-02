8ab9d77e97d3b044cb318961a3e62691419e0438
range3 <white.line.oar@gmail.com>
2021-07-02 12:53:19 +0900

    Update benchmarks/pmem2bench.sh

--- benchmarks/pmem2bench.sh
+++ benchmarks/pmem2bench.sh
@@ -3,9 +3,9 @@
 OUTPUT_DIR_PATH="$HOME/bench_results/pmem2bench"
 PMEM2BENCH_EXE="$HOME/work/spack-playground/build-RelWithDebInfo/bin/pmem2bench"
 POOL_PATH=/mnt/pmem0/$USER/pmem2bench.pool
-POOL_SIZE=64G
-TOTAL_SIZE=64G
-LABEL=$(date '+%F_%T')-64G-non-temporal
+POOL_SIZE=8G
+TOTAL_SIZE=8G
+LABEL=$(date '+%F_%T')-8G-all
 
 access_pattern_list=(
   ""
@@ -18,7 +18,7 @@ access_type_list=(
 )
 
 store_type_list=(
-  # ""
+  ""
   "--non-temporal"
 )
 
@@ -30,12 +30,15 @@ for store_type in "${store_type_list[@]}"; do
 for access_pattern in "${access_pattern_list[@]}"; do
 for nthreads in 1 2 4 8 16; do
 for block_size in 64 128 256 512 1K 2K 4K 8K 16K 32K 64K 128K 256K 512K 1M; do
+# block_size_byte=$(echo $block_size | numfmt --from=iec)
+# stripe_size=$((block_size_byte * nthreads))
 for access_type in "${access_type_list[@]}"; do
 for iteration in 0 1 2; do
 
 OUTPUT_FILE="${TOTAL_SIZE}_${block_size}_${nthreads}_${access_type}_${access_pattern}_${store_type}_${iteration}"
 echo $OUTPUT_FILE
 
+  # --stripe $stripe_size
 $PMEM2BENCH_EXE \
   --file $POOL_PATH \
   --file_size $POOL_SIZE \
