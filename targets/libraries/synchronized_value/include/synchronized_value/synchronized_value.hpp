7a044ff47c029e7bc548ce76034b9c3520fe166e
range3 <white.line.oar@gmail.com>
2021-12-14 17:22:26 +0900

    Remove c++20 concepts from SynchronizedValue<T>
    instead using a partial specialization of the template

--- targets/libraries/synchronized_value/include/synchronized_value/synchronized_value.hpp
+++ targets/libraries/synchronized_value/include/synchronized_value/synchronized_value.hpp
@@ -16,7 +16,7 @@
 namespace range3 {
 
 template <typename T, typename Lockable = std::mutex>
-class SynchronizedValue {
+class SynchronizedValueImpl {
  public:
   using value_type = T;
   using mutex_type = Lockable;
@@ -28,7 +28,8 @@ class SynchronizedValue {
  public:
   class ConstLockGuardPtr : public std::lock_guard<mutex_type> {
    public:
-    using synchronized_value_type = SynchronizedValue<value_type, mutex_type>;
+    using synchronized_value_type =
+        SynchronizedValueImpl<value_type, mutex_type>;
 
     ConstLockGuardPtr(synchronized_value_type const& sync_value)
         : std::lock_guard<mutex_type>(sync_value.mutex_),
@@ -65,7 +66,8 @@ class SynchronizedValue {
     using base_type = std::unique_lock<mutex_type>;
 
    public:
-    using synchronized_value_type = SynchronizedValue<value_type, mutex_type>;
+    using synchronized_value_type =
+        SynchronizedValueImpl<value_type, mutex_type>;
 
     ConstUniqueLockPtr(synchronized_value_type const& sync_value)
         : base_type(sync_value.mutex_), value_(sync_value.value_){};
@@ -96,36 +98,36 @@ class SynchronizedValue {
     }
   };
 
-  SynchronizedValue() = default;
-  SynchronizedValue(value_type const& other) : value_(other) {}
-  SynchronizedValue(value_type&& other) : value_(std::move(other)) {}
-  SynchronizedValue(SynchronizedValue const& other) requires
-      std::is_copy_constructible_v<value_type>  // C++20 :(
-      : SynchronizedValue(other, std::lock_guard<mutex_type>(other.mutex_)) {}
-  SynchronizedValue(SynchronizedValue&& other)
-      : SynchronizedValue(std::move(other),
-                          std::lock_guard<mutex_type>(other.mutex_)) {}
+  SynchronizedValueImpl() = default;
+  SynchronizedValueImpl(value_type const& other) : value_(other) {}
+  SynchronizedValueImpl(value_type&& other) : value_(std::move(other)) {}
+  SynchronizedValueImpl(SynchronizedValueImpl const& other)
+      : SynchronizedValueImpl(other,
+                              std::lock_guard<mutex_type>(other.mutex_)) {}
+  SynchronizedValueImpl(SynchronizedValueImpl&& other)
+      : SynchronizedValueImpl(std::move(other),
+                              std::lock_guard<mutex_type>(other.mutex_)) {}
 
-  auto operator=(SynchronizedValue const& rhs) -> SynchronizedValue& {
+  auto operator=(SynchronizedValueImpl const& rhs) -> SynchronizedValueImpl& {
     if (&rhs != this) {
       std::scoped_lock lock{mutex_, rhs.mutex_};
       value_ = rhs.value_;
     }
     return *this;
   }
-  auto operator=(SynchronizedValue&& rhs) -> SynchronizedValue& {
+  auto operator=(SynchronizedValueImpl&& rhs) -> SynchronizedValueImpl& {
     if (&rhs != this) {
       std::scoped_lock lock{mutex_, rhs.mutex_};
       value_ = std::move(rhs.value_);
     }
     return *this;
   }
-  auto operator=(value_type const& other) -> SynchronizedValue& {
+  auto operator=(value_type const& other) -> SynchronizedValueImpl& {
     std::lock_guard<mutex_type> lock(mutex_);
     value_ = other;
     return *this;
   }
-  auto operator=(value_type&& other) -> SynchronizedValue& {
+  auto operator=(value_type&& other) -> SynchronizedValueImpl& {
     std::lock_guard<mutex_type> lock(mutex_);
     value_ = std::move(other);
     return *this;
@@ -136,7 +138,7 @@ class SynchronizedValue {
   }
   explicit operator T() const { return get(); }
 
-  void swap(SynchronizedValue& rhs) {
+  void swap(SynchronizedValueImpl& rhs) {
     if (this == &rhs) {
       return;
     }
@@ -169,12 +171,75 @@ class SynchronizedValue {
   auto operator->() const -> ConstLockGuardPtr { return synchronize(); }
 
  private:
-  SynchronizedValue(SynchronizedValue const& other,
-                    std::lock_guard<mutex_type> const&)
+  SynchronizedValueImpl(SynchronizedValueImpl const& other,
+                        std::lock_guard<mutex_type> const&)
       : value_(other.value_) {}
-  SynchronizedValue(SynchronizedValue&& other,
-                    std::lock_guard<mutex_type> const&)
+  SynchronizedValueImpl(SynchronizedValueImpl&& other,
+                        std::lock_guard<mutex_type> const&)
       : value_(std::move(other.value_)) {}
+};
+
+template <typename T,
+          typename Lockable = std::mutex,
+          bool = std::is_copy_constructible<T>::value>
+class SynchronizedValue : public SynchronizedValueImpl<T, Lockable> {
+ public:
+  using value_type = T;
+  using mutex_type = Lockable;
+
+  SynchronizedValue() = default;
+  SynchronizedValue(value_type const& other)
+      : SynchronizedValueImpl<T, Lockable>(other) {}
+  SynchronizedValue(value_type&& other)
+      : SynchronizedValueImpl<T, Lockable>(std::move(other)) {}
+  SynchronizedValue(SynchronizedValue const& other)
+      // requires std::is_copy_constructible_v<value_type> C++20 feature
+      : SynchronizedValueImpl<T, Lockable>(other) {}
+  SynchronizedValue(SynchronizedValue&& other)
+      : SynchronizedValueImpl<T, Lockable>(std::move(other)) {}
+
+  auto operator=(SynchronizedValue const& rhs) -> SynchronizedValue& {
+    SynchronizedValueImpl<T, Lockable>::operator=(rhs);
+    return *this;
+  }
+  auto operator=(SynchronizedValue&& rhs) -> SynchronizedValue& {
+    SynchronizedValueImpl<T, Lockable>::operator=(std::move(rhs));
+    return *this;
+  }
+};
+
+// If T is noncopyable but movable, SynchronizedValue<T> (or synch<T>) becomes
+// also noncopyable but movable. The problem is std::vector<synch<T>> causes
+// compile error even if std::vector<T> supports noncopyable movable T. e.g.
+// std::vector<std::unique_ptr<T>> is available, but
+// std::vector<synch<std::unique_ptr<T>>> isn't.
+// Because std::is_copy_constructible<synch<T>>::value becomes true
+// even as synch<T> is actually noncopyable.
+// std::vector determines whether copy or move
+// constructor should be used by this condition, not by SFINAE.
+// https://stackoverflow.com/questions/62110426/why-does-code-with-stdvector-not-compile-but-with-stdunique-ptr-it-does-if
+// To solve this problem, when T is uncopyable,
+// we use a partial specialization of the template so that synch<T> does not
+// have copy ctor and assignment operator.
+
+template <typename T, typename Lockable>
+class SynchronizedValue<T, Lockable, false>
+    : public SynchronizedValue<T, Lockable, true> {
+ public:
+  using value_type = T;
+  using mutex_type = Lockable;
+
+  SynchronizedValue() = default;
+  SynchronizedValue(value_type&& other)
+      : SynchronizedValue<T, Lockable, true>(std::move(other)) {}
+  SynchronizedValue(SynchronizedValue const& other) = delete;
+  SynchronizedValue(SynchronizedValue&& other)
+      : SynchronizedValue<T, Lockable, true>(std::move(other)) {}
+
+  auto operator=(SynchronizedValue&& rhs) -> SynchronizedValue& {
+    SynchronizedValueImpl<T, Lockable>::operator=(std::move(rhs));
+    return *this;
+  }
 };
 
 // template <typename SV1, typename SV2>
@@ -210,26 +275,28 @@ auto synchronize(SV&... sv) -> std::tuple<
 }
 
 template <typename T, typename L>
-inline void swap(SynchronizedValue<T, L>& lhs, SynchronizedValue<T, L>& rhs) {
+inline void swap(SynchronizedValueImpl<T, L>& lhs,
+                 SynchronizedValueImpl<T, L>& rhs) {
   lhs.swap(rhs);
 }
 template <typename T, typename L>
-inline void swap(SynchronizedValue<T, L>& lhs, T& rhs) {
+inline void swap(SynchronizedValueImpl<T, L>& lhs, T& rhs) {
   lhs.swap(rhs);
 }
 template <typename T, typename L>
-inline void swap(T& lhs, SynchronizedValue<T, L>& rhs) {
+inline void swap(T& lhs, SynchronizedValueImpl<T, L>& rhs) {
   rhs.swap(lhs);
 }
 
 template <typename OStream, typename T, typename L>
-inline auto operator<<(OStream& os, SynchronizedValue<T, L> const& rhs)
+inline auto operator<<(OStream& os, SynchronizedValueImpl<T, L> const& rhs)
     -> OStream& {
   rhs.save(os);
   return os;
 }
 template <typename IStream, typename T, typename L>
-inline auto operator>>(IStream& is, SynchronizedValue<T, L>& rhs) -> IStream& {
+inline auto operator>>(IStream& is, SynchronizedValueImpl<T, L>& rhs)
+    -> IStream& {
   rhs.load(is);
   return is;
 }
